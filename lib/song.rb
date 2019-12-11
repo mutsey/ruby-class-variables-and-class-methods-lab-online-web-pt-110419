20eceb1152f0e59519fb23def9f0922aeb2ab42d
Meagan Utsey <meagan.utsey@gmail.com>
2019-12-11 04:34:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,7 @@ ninety_nine_problems.artist
 ninety_nine_problems.genre
   # => "rap"
   
-  hit_me = Song.new("99 Problems", "Jay-Z", "rap")
+  hit_me = Song.new("hit me baby one more time", "Brittany Spears", "pop")
  
  :hit_me) { Song.new("hit me baby one more time", "Brittany Spears", "pop")
  
