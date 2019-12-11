acb34e0d2f521b9a995c9afbfdec5ef2dfd92600
Meagan Utsey <meagan.utsey@gmail.com>
2019-12-11 04:36:39 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+lucifier = Song.new("lucifier", "Jay-Z", "rap")
+ 
+lucifier.name
+  # => "lucifier"
+ 
+lucifier.artist
+  # => "Jay-Z"
+ 
+lucifier.genre
+  # => "rap"
+  
+  ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+ 
+ninety_nine_problems.name
+  # => "99 Problems"
+ 
+ninety_nine_problems.artist
+  # => "Jay-Z"
+ 
+ninety_nine_problems.genre
+  # => "rap"
+  
+  hit_me = Song.new("hit me baby one more time", "Brittany Spears", "pop")
+ 
+hit_me.name
+  # => "hit me baby one more time" 
+ 
+hit_me.artist
+  # => "Brittany Spears"
+ 
+hit_me.genre
+  # => "pop"
